804b60237c23e76acb31246dd44ed2e5178e9312
mdlsnts <chmielowiec.marta@gmail.com>
2023-09-07 16:33:08 +0200

    set basic functionality
    - add typed value as new li
    - clear placeholder

---
+++ dist/index.js
@@ -0,0 +1,26 @@
+"use strict";
+// "const btn = document.getElementById("btn")!;" <-- quaranteed that it's not gonna be null
+const btn = document.getElementById("btn");
+const input = document.getElementById("todoinput");
+const form = document.querySelector("form");
+const list = document.getElementById("todolist");
+// works also as:
+// (<HTMLInputElement>input).value <-- doesn't work with REACT
+// btn.addEventListener("click", function () {});
+//"?" after the element will notify Js that IF that element exist execute it
+// btn.addEventListener("click", function () {
+//   alert(input.value);
+//   input.value = "";
+// });
+function handleSubmit(e) {
+    e.preventDefault();
+    const newToDoText = input.value;
+    const newLI = document.createElement("li");
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    newLI.append(checkbox);
+    newLI.append(newToDoText);
+    list.append(newLI);
+    input.value = "";
+}
+form.addEventListener("submit", handleSubmit);
