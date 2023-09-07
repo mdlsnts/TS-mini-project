31ac420116451ab1f58312530b098161aac1faf6
mdlsnts <chmielowiec.marta@gmail.com>
2023-09-07 16:45:30 +0200

    stored li's as an array

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,9 @@
 "use strict";
-// "const btn = document.getElementById("btn")!;" <-- quaranteed that it's not gonna be null
 const btn = document.getElementById("btn");
 const input = document.getElementById("todoinput");
 const form = document.querySelector("form");
 const list = document.getElementById("todolist");
+const todos = [];
 // works also as:
 // (<HTMLInputElement>input).value <-- doesn't work with REACT
 // btn.addEventListener("click", function () {});
@@ -14,13 +14,20 @@ const list = document.getElementById("todolist");
 // });
 function handleSubmit(e) {
     e.preventDefault();
-    const newToDoText = input.value;
+    const newTodo = {
+        text: input.value,
+        status: false,
+    };
+    createTodo(newTodo);
+    todos.push(newTodo);
+    input.value = "";
+}
+function createTodo(todo) {
     const newLI = document.createElement("li");
     const checkbox = document.createElement("input");
     checkbox.type = "checkbox";
     newLI.append(checkbox);
-    newLI.append(newToDoText);
+    newLI.append(todo.text);
     list.append(newLI);
-    input.value = "";
 }
 form.addEventListener("submit", handleSubmit);
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,14 @@
 // "const btn = document.getElementById("btn")!;" <-- quaranteed that it's not gonna be null
+interface Todo {
+  text: string;
+  status: boolean;
+}
+
 const btn = document.getElementById("btn")! as HTMLButtonElement;
 const input = document.getElementById("todoinput")! as HTMLInputElement;
 const form = document.querySelector("form")!;
 const list = document.getElementById("todolist")!;
+const todos: Todo[] = [];
 
 // works also as:
 // (<HTMLInputElement>input).value <-- doesn't work with REACT
@@ -16,14 +22,22 @@ const list = document.getElementById("todolist")!;
 
 function handleSubmit(e: SubmitEvent) {
   e.preventDefault();
-  const newToDoText = input.value;
+  const newTodo: Todo = {
+    text: input.value,
+    status: false,
+  };
+  createTodo(newTodo);
+  todos.push(newTodo);
+  input.value = "";
+}
+
+function createTodo(todo: Todo) {
   const newLI = document.createElement("li");
   const checkbox = document.createElement("input");
   checkbox.type = "checkbox";
   newLI.append(checkbox);
-  newLI.append(newToDoText);
+  newLI.append(todo.text);
   list.append(newLI);
-  input.value = "";
 }
 
 form.addEventListener("submit", handleSubmit);
